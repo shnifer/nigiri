cfc5dbaefd5f523e6c9473c261304bafbb69be3e
Shnifer <11@a.com>
2018-08-29 17:43:29 +0300

    start

--- samples/sprite.go
+++ samples/sprite.go
@@ -2,31 +2,30 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten"
-	"github.com/Shnifer/nigiri"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"image"
 )
 
-var Q *nigiri.Queue
-var S *nigiri.Sprite
+var T *ebiten.Image
 
 func mainLoop(win *ebiten.Image) error{
-	Q.Clear()
-	Q.Add(S)
-	Q.Run(win)
+	G:=ebiten.GeoM{}
+	G.Rotate(0.1)
+	src:=image.Rect(100,100,350,350)
+	do:=&ebiten.DrawImageOptions{
+		GeoM: G,
+		Filter:ebiten.FilterLinear,
+		SourceRect:&src,
+	}
+	win.DrawImage(T,do)
 	return nil
 }
 
 func main(){
-	nigiri.StartProfile("sprite")
-	defer nigiri.StopProfile("sprite")
-
-	Q = nigiri.NewQueue(nil)
-	nigiri.SetTexLoader(nigiri.FileTexLoader("samples"))
-	tex, err:=nigiri.GetTex("HUD_Ship.png")
+	tex,_,err:=ebitenutil.NewImageFromFile("samples/HUD_Ship.png",ebiten.FilterDefault)
+	T = tex
 	if err!=nil{
 		panic(err)
 	}
-	src:=nigiri.NewStatic(tex, nil, "tag")
-	S = nigiri.NewSprite(src, nigiri.Scaler{Sx:1, Sy:1})
-	S.SetSmooth(true)
 	ebiten.Run(mainLoop, 600,600,1,"TEST")
 }
